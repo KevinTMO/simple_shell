908800af2083d2abb604869ab2a56235c9dd2330
KevinTMO <kevin.ramos3@hotmail.com>
2021-04-15 00:50:27 +0000

    Adding shell functions, source code && header

---
+++ shell.h
@@ -0,0 +1,56 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdio_ext.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdarg.h>
+#include <errno.h>
+
+/* DEFINING MACROS BELOW */
+
+#define MAX_LEN_CMD 105
+#define MAX_ARGS 13
+
+/* DECLARING GLOBAL VARIABLE EXTENSION */
+
+extern char **environ;
+
+/* KEVINTMO PROTOTYPES BELOW */
+
+void __echo(char *args[], char *PWD, char *HOME, char *SHELL, char *PATH);
+void externCMD(char *cmd, char *args[]);
+void extracTokens(char *cmd, char *args[]);
+void dirList(char *PWD, char *args[]);
+
+/* ORIGINAL FUNCTIONS BELOW */
+
+void comExterno(void);
+void eco(void);
+void listaDir(void);
+void separaArgs(void);
+
+/* AUX FUNCTIONS BELOW */
+
+int _putchar(char c);
+unsigned int _strlen(char *s);
+int _putstring(char *s);
+int _strcmp(char *s1, char *s2);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _printf(const char *format, ...);
+char *_getenv(char *pathName);
+char *_memset(char *s, char b, unsigned int n);
+
+/* EXECUTABLE FUNCTIONS */
+
+void _execd(char *args[], char *PWD);
+void _exeEcho(char *args[], char *PWD, char *HOME, char *SHELL, char *PATH);
+void _exeEnv(char *cmd, char *HOME, char *PWD, char *SHELL, char *PATH);
+
+#endif /* SHELL_H */
---
+++ shellFun.c
@@ -0,0 +1,114 @@
+#include "shell.h"
+
+/**
+ *extracTokens - will replace delim character for null characters
+ *@cmd: is the parameter to the command
+ *@args: is the arguments of strings
+ *Return: will return nothing (void)
+ */
+
+void extracTokens(char *cmd, char *args[])
+{
+	int index;
+	/* @index: will iterate to eliminate the last argument */
+
+	for (index = 0; index < (MAX_ARGS - 1); index++)
+	{
+		args[index] = NULL;
+	}
+
+	strtok(cmd, " ");
+	index = 0;
+
+	args[index] = cmd;
+	while ((args[++index] = strtok(NULL, " ")) != NULL && index < (MAX_ARGS - 2))
+	{
+		;
+	}
+}
+
+/**
+ *__echo - will print words to the stdout
+ *@args: is the arguments of strings
+ *@PWD: is the working dir
+ *@HOME: is the home working dir
+ *@SHELL: is the working dir on the shell
+ *@PATH: is holding the path
+ *Return: will return nothing (void)
+ */
+
+void __echo(char *args[], char *PWD, char *HOME, char *SHELL, char *PATH)
+{
+	unsigned int idxC, idxStr = 0;
+	int idxT;
+	char tmp[6];
+	/* @idxC: will iterate the characters of the strings of the arguments */
+	/* @idxT: will iterate the characters of the string tmp */
+	/* @idxStr: will iterate the strings of the arguments */
+	/* @tmp: will hold the string to compare */
+
+	while (args[++idxStr])
+	{
+		for (idxC = 0; idxC < _strlen(args[idxStr]); idxC++)
+		{
+			if (args[idxStr][idxC] != '$')
+				_printf("%c", args[idxStr][idxC]);
+			else
+			{
+				idxT = -1;
+				while (++idxT < 5 && (idxC + idxT + 1) < _strlen(args[idxStr])
+				       && args[idxStr][idxC + idxT + 1] != '\0')
+				{
+					tmp[idxT] = args[idxStr][idxC + idxT + 1];
+				}
+				tmp[idxT] = '\0';
+				if (_strcmp(tmp, "SHELL") == 0)
+					_printf("%s", SHELL), idxC += 5;
+				else if (_strcmp(tmp, "PATH") == 0)
+					_printf("%s", PATH), idxC += 4;
+				else if (_strcmp(tmp, "PWD") == 0)
+					_printf("%s", PWD), idxC += 3;
+				else if (_strcmp(tmp, "HOME") == 0)
+					_printf("%s", HOME), idxC += 4;
+				else
+					_printf("$");
+			}
+		}
+		_printf(" ");
+	}
+	_printf("\n");
+}
+
+/**
+ *externCMD - will access the commands of the environment
+ *@cmd: is the input command
+ *@args: is the arguments of strings
+ *Return: will return nothing (void)
+ */
+
+void externCMD(char *cmd, char *args[])
+{
+	int pid = 0; /* @pid: is the variable of the process identification */
+	int status; /* is the variable that will receive the return val of execvp */
+
+	if (_strcmp(cmd, "cd") == 0 || _strcmp(cmd, "environ") == 0)
+		return;
+
+	pid = fork();
+
+	if (pid < 0)
+		_printf("Error: the child process couldn't be created\n");
+
+	if (pid == 0)
+	{
+		status = execvp(cmd, args);
+
+		if (status)
+		{
+			_printf("Error: it couldn't recognize %s or couldn't execute\n", cmd);
+			exit(1);
+		}
+	}
+	else
+		wait(NULL);
+}
